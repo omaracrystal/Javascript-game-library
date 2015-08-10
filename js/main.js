9232ff7c56fa4e7e9fdd5470b83d70d793947c34
omaracrystal <omaracrystal@gmail.com>
2015-08-10 10:45:25 -0600

    created Game class

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// add scripts
+
+$(document).on('ready', function() {
+  console.log('sanity check!');
+});
+
+
+var Game = function (title, genre) {
+  this.title = title;
+  this.genre = genre;
+}
+
