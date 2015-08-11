cb2b31a7f5e142db7b12c8272ad28bdfcf95e5ee
omaracrystal <omaracrystal@gmail.com>
2015-08-11 13:42:56 -0600

    created an object array of games

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // add scripts
-
 $(document).on('ready', function() {
   console.log('sanity check!');
   $('body').append("<div class='form-group'> <form id='myForm' class= 'form-control'><b>TITLE:</b><input type='text' name='title' data= 'title' required><br><br><b>GENRE:</b><input type= 'text' name= 'genre' data= 'genre' required><br><br><input type='submit'><br><br><table class= 'table table-bordered'><tr><th><b>TITLE</b></th><th><b>GENRE</b></th></tr></table></div>")
@@ -7,11 +6,9 @@ $(document).on('ready', function() {
     // .append(cribbage.render())
 
 
-
-
 var Game = function (title, genre) {
-  this.title = title || undefined;
-  this.genre = genre || undefined;
+  this.title = title;
+  this.genre = genre;
 };
 
 
@@ -22,13 +19,13 @@ var GameLibrary = function (title) {
 }
 
 
-// GameLibrary.prototype.addGame = funtion(game) {
-//   this.games.push(game);
-// };
+GameLibrary.prototype.addGame = function(game) {
+  this.games.push(game);
+};
 
 
-Game.prototype.render = function(title, genre){
-  this.$element = $("<table class='table table-bordered'>")
+Game.prototype.render = function(){
+  this.$element = $("tbody")
     .append('<tr><td>'+ this.title + '</td><td>' +this.genre + '</td></tr>')
     return this.$element;
 }
@@ -37,25 +34,29 @@ var gameArray = [];
 
 $('#myForm').on('submit', function(e) {
   e.preventDefault();
-  console.log('test');
-//Get all the forms elements and their values in one step
-  // var values = $(this).serialize();
-  var obj = {};
-    $(':input:lt(2)').each(function() {
-      obj.push($(this).name + $(this).val());
-});
-  gameArray.push(obj)
+
+  //grab values
+  var title = $('input:eq(0)').val();
+  var genre = $('input:eq(1)').val();
+  $('.table').append('<tr><td>'+title+'</td><td>'+genre+'</td></tr>')
+  //iterate over table titles and genres
+  for (var i = 0; i < $("tbody").length; i++) {
+    // if (title.indexOf && genre.indexOf === -1) {
+      gameArray.push({Title: title, Genre: genre});
+
+    // }
+  }
+
   console.log(gameArray);
+
 });
 
+});
 
-// $('#myForm').bind('submit', function () {
-//   var elements = this.elements;
+// Game.prototype.addGame = function(title, genre) {
+//   for (var i = 0; i < tbody.length; i++) {
+//     if (title.indexOf && genre.indexOf === -1)
+//   };
+// }
 // });
 
-// new Game($', 'Board Game');
-// console.log(chess);
-
-var cribbage = new Game('Cribbage', 'Card Game')
-
-});
